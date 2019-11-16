865bb27681c929095f8a44233f8d7c6beda76b3d
luka-kovacevic <51401592+luka-kovacevic@users.noreply.github.com>
2019-11-16 23:10:28 +0000

    Update Graph.py

--- Hackathon/Graph.py
+++ Hackathon/Graph.py
@@ -8,36 +8,41 @@ Created on Sat Nov 16 12:54:10 2019
 '''
 import numpy as np
 import googlemaps
+import postcodes
 
 class Graph:
-        
+
     #for a given list of postcodes, creates the incidence matrix for the distances between them
     def __init__(self, postcodeList):
         aKey = 'AIzaSyDXKLWHJQdqzVI1agSREbzr4AuoBKyUeuE'
-        
+
+        if len(postcodeList[0]) > 1:
+            for x in range(0, len(postcodeList)):
+                postcodeList[x] = postcodes.convertCoordToPost(postcodeList[x])
+
         dest = postcodeList[0].split(' ')
         destList = dest[0]+'+'+dest[1]
-        
+
         for x in range(1, len(postcodeList)):
             dest = postcodeList[x].split(' ')
             destPostcode = dest[0]+'+'+dest[1]
             destList = destList+'|'+destPostcode
-                       
+
         self.destinations = destList
-            
+
         gmaps = googlemaps.Client(key = aKey)
         self.distanceDict = gmaps.distance_matrix(origins = destList , destinations = destList, mode = 'walking')
-        
+
         n = len(self.distanceDict['rows'])
         self.incidence = np.eye(n)
-        
+
         for x in range(0, n):
             elem = self.distanceDict['rows'][x]
             for y in range(0, len(elem['elements'])):
                 self.incidence[x][y] = elem['elements'][y]['distance']['value']
-        
-    def getIncidence(self):        
+
+    def getIncidence(self):
         return self.incidence
-    
+
     def getDestList(self):
         return self.destinations
