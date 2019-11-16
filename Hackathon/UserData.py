bda0526bbc413b78dca29b0dd8c1d3ab44423d11
Benjamin Rees <br7g18@soton.ac.uk>
2019-11-16 23:30:10 +0000

    added new parameter of keyword, with default argument bar

--- Hackathon/UserData.py
+++ Hackathon/UserData.py
@@ -1,12 +1,16 @@
 
 #encapsulates the data inputted from the user
 class UserData :
-	def __init__(self, location,distance) :
+	def __init__(self, location,distance,keyword="bar") :
 		self.location = location
 		self.distance = distance
+		self.keyword = keyword
 
-	def getLocation() :
+	def getLocation(self) :
 		return location
 
-	def getDistance() :
+	def getDistance(self) :
 		return distance
+
+	def getKeyword(self)
+		return keyword
