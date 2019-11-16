3266670e8221217c8a3d35bed228fb1c85efeb05
luka-kovacevic <51401592+luka-kovacevic@users.noreply.github.com>
2019-11-16 23:10:54 +0000

    Update postcodes.py

--- Hackathon/postcodes.py
+++ Hackathon/postcodes.py
@@ -1,18 +1,18 @@
 
 
 #jdata = getPlaces("bar", "51.7520220,-1.2577260", "100")
-from geopy.geocoders import Nominatim    
+from geopy.geocoders import Nominatim
 
-class postcodes:  
+class postcodes:
 
     def __init__(self):
         super().__init__()
-    
+
     # a method which takes in a json object and searches through it to output the latitude and longitude of the location
     def address(self, json_object):
-        
-        l = [] 
-        
+
+        l = []
+
         if json_object == None:
             return None
         else:
@@ -21,9 +21,14 @@ class postcodes:
         return l
 
     #takes postcode as argument, returns coordinates
-    def convertPostCoord(self, postcode):
+    def convertPostToCoord(self, postcode):
         geolocator = Nominatim(user_agent='postcodeConverter')
         location = geolocator.geocode(postcode)
         return location.latitude, location.longitude
+
+    def convertCoordToPost(self, coord):
+        geolocator = Nominatim(user_agent="coordConverter")
+        location = geolocator.reverse(coord[0], coord[1])
+        return location
 
 # address(jdata)
