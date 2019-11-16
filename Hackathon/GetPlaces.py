d99148bf362a9aa0700fffeb2ba9ae42e4629ce2
Dave <dew1g18@soton.ac.uk>
2019-11-16 22:39:32 +0000

    dunno, inavlid requests or some shit

--- Hackathon/GetPlaces.py
+++ Hackathon/GetPlaces.py
@@ -15,17 +15,18 @@ class GetPlaces:
         params = "fields=formatted_address&"
         url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + startLocation+ inputPart + fieldsPart +radius +params+"key=AIzaSyDXKLWHJQdqzVI1agSREbzr4AuoBKyUeuE"    
 
-        # print (url)
+        print (url)
         # with urllib.request.urlopen(urlToRead) as url:
         #     data = json.loads(url.read().decode())
         #     print(data)
         response = urllib.request.urlopen(url)
 
         data = json.loads(response.read())
-
+        # print (data)
         # print (data['formatted_address'])
         return (data)
 
 
 
-# print(GetPlaces.getPlaces("bar", "51.7520220,-1.2577260", "50"))
+# print(GetPlaces.getPlaces("bar", "51.7520220,-1.2577260", "50")
+
