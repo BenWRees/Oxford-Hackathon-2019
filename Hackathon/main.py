56fc8558be8685b35f1c9945a205b16488e201fe
Dave <dew1g18@soton.ac.uk>
2019-11-16 22:38:14 +0000

    chloes is implemented in main

--- Hackathon/main.py
+++ Hackathon/main.py
@@ -10,11 +10,16 @@ import json
 
 
 def main():
-    # jsondat = GetPlaces.getPlaces(sys.argv[0], sys.argv[1], sys.argv[2])
+    jsondat = gp.getPlaces(sys.argv[1], sys.argv[2], sys.argv[3])
+    # jsondat = gp.getPlaces("bar", "51.7520220,-1.2577260", "50")
+    # print (sys.argv[1])
+    # print (jsondat)
+    parseList = post.address(jsondat)
+    print (parseList)
 
-    jsondat = gp.getPlaces("bar", "51.7520220,-1.2577260", "50")
-    print (jsondat)
+
 
 if __name__ == "__main__":    
     gp = GetPlaces.GetPlaces()
+    post = postcodes.postcodes()
     main()
