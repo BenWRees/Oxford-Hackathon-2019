8d57b1cb0a63b58ac6496f6bbe5a125f09628166
Benjamin Rees <br7g18@soton.ac.uk>
2019-11-17 04:06:14 +0000

    cool new features - I swear they're not bugs, they're 'features'

--- Hackathon/GoogleRoutePlanner.py
+++ Hackathon/GoogleRoutePlanner.py
@@ -1,5 +1,4 @@
 import googlemaps
-import UserData
 import postcodes
 import IndiceToPostcodeConverter
 import Graph
@@ -14,34 +13,31 @@ class GoogleRoutePlanner :
 		#self.postcodes = postcodes.Postcodes()
 		gmaps = googlemaps.Client(key = aKey)
 
-	#function that turns the list of postcodes gathered from RoutePostcodeConverter to a list of addresses
-	def postcodesAddressConverter(self.postcodes,namesList) :
-		#for currentPostcode in postcodes :
-		listofAddresses = []
-		graph = Graph.Graph(postcodes,namesList)
-		for currentPostcode in postcodes :
-			newLatLong = graph.convertPostToCoord(currentPostcode)
-			listofAddresses.append(newLatLong)
-		return listofAddresses
 
 	#adds the origin and destination to the address
-	def addOriginDestination(self,latitudes,address):
-		latLong = latitudes[0]
-		latString = str(latLong[0])
-		longString = str(latLong[1])
-		address = address +latString + "," + longString + "&" + "destination=" +latString + "," + longString + "&" + "travelmode=walking&waypoints="
+	def addOriginDestination(self,names, address):
+		origin = names[0]
+		origin = list(origin)
+		origin = origin[0].split()
+		lat_address = str(origin[0])
+		long_address = str(origin[1])
+		address = address + lat_address + "," + long_address + "&" + "destination=" + lat_address + "," + long_address + "&" + "travelmode=walking&waypoints="
+		#address=""
 		return address
 
 	#adds waypoints to the address
-	def addWaypoints(self,latitudes,address) :
+	def addWaypoints(self,names,address) :
 		#remove first and last elements (start and finish)
-		latitudes.pop()
-		del latitudes[0]
+		names.pop()
+		del names[0]
 
-		for currentLatitude in latitudes :
-			latString = str(currentLatitude[0])
-			longString = str(currentLatitude[1])
-			address = address + "|" + latString + "," + longString
+		for currentName in names :
+			currentName = list(currentName)
+			currentName = str(currentName)
+			currentName = currentName.replace(" ", "+")
+			currentName = currentName.replace("[", "")
+			currentName = currentName.replace("]", "")
+			address = address + "|" + currentName
 		return address
 
 	#function that will return the completed address with full route on Google
@@ -53,10 +49,10 @@ class GoogleRoutePlanner :
 	"""
 
 googleRoute = GoogleRoutePlanner() 
-postcodes = ["TN21 0TQ","RG1 6PF","RM11 2EH","S017 1AW","SO16 4UF","SO18 2NU","SO17 3RE","HS7 5PG","GL5 1JY","L22 7RA","OX14 5JZ","RG19 8BT","TN32 5BP"]
-latitudes = googleRoute.postcodesAddressConverter(postcodes)
-startOfAddress = googleRoute.addOriginDestination(latitudes, "https://www.google.com/maps/dir/?api=1&origin=")
-print(googleRoute.ddWaypoints(latitudes,startOfAddress))
+postcodes = [{"home"},{"The Bear Inn"},{"The Crown Inn"},{"The Mitre"},{"Clowns Wine Bar"},{"The Hobbit Inn"},{"Wild Lime"},{"The Shooting Star"},{"home"}]
+#latitudes = googleRoute.postcodesAddressConverter(postcodes)
+startOfAddress = googleRoute.addOriginDestination(postcodes, "https://www.google.com/maps/dir/?api=1&origin=")
+print(googleRoute.addWaypoints(postcodes,startOfAddress))
 
 
 
