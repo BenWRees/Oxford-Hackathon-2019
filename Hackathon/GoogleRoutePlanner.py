c8c6ba77d5ba544704fb4a6f671208da25085ace
Benjamin Rees <br7g18@soton.ac.uk>
2019-11-17 04:26:34 +0000

    remove unnecessary comment

--- Hackathon/GoogleRoutePlanner.py
+++ Hackathon/GoogleRoutePlanner.py
@@ -17,12 +17,9 @@ class GoogleRoutePlanner :
 	#adds the origin and destination to the address
 	def addOriginDestination(self,names, address):
 		origin = names[0]
-		origin = list(origin)
-		origin = origin[0].split()
 		lat_address = str(origin[0])
 		long_address = str(origin[1])
 		address = address + lat_address + "," + long_address + "&" + "destination=" + lat_address + "," + long_address + "&" + "travelmode=walking&waypoints="
-		#address=""
 		return address
 
 	#adds waypoints to the address
@@ -49,7 +46,7 @@ class GoogleRoutePlanner :
 	"""
 
 googleRoute = GoogleRoutePlanner() 
-postcodes = [{"home"},{"The Bear Inn"},{"The Crown Inn"},{"The Mitre"},{"Clowns Wine Bar"},{"The Hobbit Inn"},{"Wild Lime"},{"The Shooting Star"},{"home"}]
+postcodes = [(50.5,-1.7),{"The Bear Inn"},{"The Crown Inn"},{"The Mitre"},{"Clowns Wine Bar"},{"The Hobbit Inn"},{"Wild Lime"},{"The Shooting Star"},{"home"}]
 #latitudes = googleRoute.postcodesAddressConverter(postcodes)
 startOfAddress = googleRoute.addOriginDestination(postcodes, "https://www.google.com/maps/dir/?api=1&origin=")
 print(googleRoute.addWaypoints(postcodes,startOfAddress))
