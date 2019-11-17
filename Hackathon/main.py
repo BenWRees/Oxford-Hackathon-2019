810db164cd54fc70207c08ec23ac98a796cbb763
Dave <dew1g18@soton.ac.uk>
2019-11-17 03:57:24 +0000

    Formatted output

--- Hackathon/main.py
+++ Hackathon/main.py
@@ -9,15 +9,18 @@ import json
 
 
 def main():
-    latlong = sys.argv[1]
-    jsondat = gp.getPlaces("bar", latlong, sys.argv[2])
+    postcodeInput= sys.argv[1]
+    latlong = post.convertPostToCoord(postcodeInput)
+    print(latlong)
+    latlongString = str(latlong[0])+","+str(latlong[1])
+    jsondat = gp.getPlaces("bar", latlongString, sys.argv[2])
     # jsondat = gp.getPlaces("bar", "51.7520220,-1.2577260", "50")
     # print (sys.argv[1])
     # print (jsondat)
 
     postCodeList, nameList = post.address(jsondat)
-    latlong = {float(latlong.split(',')[0]),float(latlong.split(',')[1])}
-    postCodeList = [latlong] + postCodeList
+    latlongSet = {float(latlongString.split(',')[0]),float(latlongString.split(',')[1])}
+    postCodeList = [latlongSet] + postCodeList
     nameList = ["home"]+ nameList
     # print (postCodeList)
     # print (post.convertCoordToPost([51.7521952, -1.2582522]))
@@ -35,9 +38,12 @@ def main():
         # print (nameDict[int(x)])
         pubsInOrder.append(nameDict[int(x)])
 
+
+    pubsInOrder[0]= latlong
+    pubsInOrder[-1]=latlong
     print(pubsInOrder)
+
     
-    # grp = GoogleRoutePlanner.GoogleRoutePlanner()
     # grp.createAddress()
 
 
